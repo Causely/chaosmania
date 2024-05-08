495e9132d162c95688506b0356fac969a7de2629
Steffen Geissinger <steffen@infrabox.net>
2024-05-08 15:55:04 +0200

    Add node cpu congestion scenario

---
+++ scenarios/node-cpu-congestion/run.sh
@@ -0,0 +1,19 @@
+#!/bin/sh
+
+SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
+
+IMAGE_REPO=quay.io/causely/chaosmania
+IMAGE_TAG=latest
+NAMESPACE=node-cpu-congestion
+
+helm upgrade --install --create-namespace --namespace $NAMESPACE \
+    --set image.tag=$IMAGE_TAG \
+    --set replicaCount=1 \
+    single $SCRIPT_DIR/../../helm/single 
+
+helm delete --namespace $NAMESPACE client
+helm upgrade --install --create-namespace --namespace $NAMESPACE \
+    --set image.tag=$IMAGE_TAG \
+    --set chaos.plan=/scenarios/$NAMESPACE-plan.yaml \
+    client $SCRIPT_DIR/../../helm/client
+

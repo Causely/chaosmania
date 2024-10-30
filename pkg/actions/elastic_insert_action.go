fe8e5adc13ba963b009bf23fde9d5a64a5b3f202
Steffen Geissinger <steffen@infrabox.net>
2024-10-30 13:20:28 +0100

    Add elastic search actions

---
+++ pkg/actions/elastic_insert_action.go
@@ -0,0 +1,74 @@
+package actions
+
+import (
+	"bytes"
+	"context"
+	"encoding/json"
+	"fmt"
+
+	"github.com/Causely/chaosmania/pkg"
+	"github.com/Causely/chaosmania/pkg/logger"
+	"github.com/elastic/go-elasticsearch/v8"
+	"go.uber.org/zap"
+)
+
+type ElasticInsert struct{}
+
+type ElasticInsertConfig struct {
+	Document map[string]any `json:"document"`
+	Index    string         `json:"index"`
+	Address  string         `json:"address"`
+	ApiKey   string         `json:"apikey"`
+}
+
+func (a *ElasticInsert) Execute(ctx context.Context, cfg map[string]any) error {
+	config, err := pkg.ParseConfig[ElasticInsertConfig](cfg)
+	if err != nil {
+		logger.FromContext(ctx).Warn("failed to parse config", zap.Error(err))
+		return err
+	}
+
+	es, err := elasticsearch.NewClient(elasticsearch.Config{
+		Addresses: []string{config.Address},
+		APIKey:    config.ApiKey,
+	})
+	if err != nil {
+		logger.FromContext(ctx).Warn("failed to create client", zap.Error(err))
+		return err
+	}
+
+	// Convert the document to JSON format
+	var buf bytes.Buffer
+	if err := json.NewEncoder(&buf).Encode(config.Document); err != nil {
+		logger.FromContext(ctx).Warn("failed to encode document", zap.Error(err))
+		return err
+	}
+
+	// Insert the document
+	res, err := es.Index(
+		config.Index,                 // The index name
+		&buf,                         // The document body
+		es.Index.WithDocumentID("1"), // Optional: specify a document ID
+		es.Index.WithContext(context.Background()),
+		es.Index.WithPretty(),
+	)
+	if err != nil {
+		return err
+	}
+	defer res.Body.Close()
+
+	if res.IsError() {
+		fmt.Printf("Error indexing document ID=1 to index %s: %s", config.Index, res.String())
+	} else {
+		fmt.Printf("Document indexed successfully in index %s\n", config.Index)
+	}
+	return nil
+}
+
+func (a *ElasticInsert) ParseConfig(data map[string]any) (any, error) {
+	return pkg.ParseConfig[ElasticInsertConfig](data)
+}
+
+func init() {
+	ACTIONS["ElasticInsert"] = &ElasticInsert{}
+}
---
+++ pkg/actions/elastic_query_action.go
@@ -0,0 +1,74 @@
+package actions
+
+import (
+	"context"
+	"encoding/json"
+	"fmt"
+	"strings"
+
+	"github.com/Causely/chaosmania/pkg"
+	"github.com/Causely/chaosmania/pkg/logger"
+	"github.com/elastic/go-elasticsearch/v8"
+	"go.uber.org/zap"
+)
+
+type ElasticQuery struct{}
+
+type ElasticQueryConfig struct {
+	Query   string `json:"query"`
+	Index   string `json:"index"`
+	Address string `json:"address"`
+	ApiKey  string `json:"apikey"`
+}
+
+func (a *ElasticQuery) Execute(ctx context.Context, cfg map[string]any) error {
+	config, err := pkg.ParseConfig[ElasticQueryConfig](cfg)
+	if err != nil {
+		logger.FromContext(ctx).Warn("failed to parse config", zap.Error(err))
+		return err
+	}
+
+	es, err := elasticsearch.NewClient(elasticsearch.Config{
+		Addresses: []string{config.Address},
+		APIKey:    config.ApiKey,
+	})
+	if err != nil {
+		logger.FromContext(ctx).Warn("failed to create client", zap.Error(err))
+		return err
+	}
+
+	// Perform the search request
+	res, err := es.Search(
+		es.Search.WithContext(ctx),
+		es.Search.WithIndex(config.Index),
+		es.Search.WithBody(strings.NewReader(config.Query)),
+		es.Search.WithPretty(),
+	)
+	if err != nil {
+		logger.FromContext(ctx).Warn("failed to execute search query", zap.Error(err))
+		return err
+	}
+
+	defer res.Body.Close()
+
+	// Decode and display the search results
+	var result map[string]interface{}
+	if err := json.NewDecoder(res.Body).Decode(&result); err != nil {
+		logger.FromContext(ctx).Warn("failed to decode response body", zap.Error(err))
+		return err
+	}
+
+	// Print the result in a readable format
+	fmt.Printf("Search Results:\n")
+	fmt.Println(result)
+
+	return nil
+}
+
+func (a *ElasticQuery) ParseConfig(data map[string]any) (any, error) {
+	return pkg.ParseConfig[ElasticQueryConfig](data)
+}
+
+func init() {
+	ACTIONS["ElasticQuery"] = &ElasticQuery{}
+}

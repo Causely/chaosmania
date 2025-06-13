314d4e94fc858f2ea85e2942725c76d94cf81b59
Steffen Geissinger <steffen@infrabox.net>
2025-06-14 00:00:42 +0200

    New scenario (#332)

--- scenarios/postgres-cpu-congestion/run.sh
+++ scenarios/postgres-cpu-congestion/run.sh
@@ -1,29 +1,37 @@
-#!/bin/bash
+#!/bin/sh
 
 SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
-source $SCRIPT_DIR/../../scripts/lib/chaosmania.sh
 
-# Parse arguments
-parse_args "$@"
+IMAGE_REPO=quay.io/causely/chaosmania
+IMAGE_TAG=latest
+NAMESPACE=cm-postgres-cpu-congestion
 
-# Setup scenario
-SCENARIO=cm-chained-services
+kubectl create namespace $NAMESPACE
+kubectl label namespace $NAMESPACE istio-injection=disabled --overwrite
 
-# Setup namespace
-setup_namespace $SCENARIO
-
-# Deploy single instance
-upgrade_single "frontend" $NAMESPACE $SCENARIO $SCRIPT_DIR "--set" "replicaCount=2"
-upgrade_single "payment" $NAMESPACE $SCENARIO $SCRIPT_DIR "--set" "replicaCount=2"
 
 echo "Deploying DB"
 helm upgrade --install --namespace $NAMESPACE \
     --set global.postgresql.auth.postgresPassword=postgres \
-    --set commonLabels."app\.kubernetes\.io/part-of"=$SCENARIO \
+    --set commonLabels."app\.kubernetes\.io/part-of"=$NAMESPACE \
+    --set primary.resources.limits.cpu=400m \
+    -f $SCRIPT_DIR/postgres_values.yaml \
     postgres oci://registry-1.docker.io/bitnamicharts/postgresql
     # --set commonLabels.app\.kubernetes\.io/part-of=$NAMESPACE \
 
-echo "Deploying postgres-exporter"
+helm upgrade --install --namespace $NAMESPACE \
+    --set image.tag=$IMAGE_TAG \
+    --set replicaCount=1 \
+    --set business_application=$NAMESPACE \
+    frontend $SCRIPT_DIR/../../helm/single 
+
+echo "Deploying payment"
+helm upgrade --install --namespace $NAMESPACE \
+    --set image.tag=$IMAGE_TAG \
+    --set replicaCount=2 \
+    --set business_application=$NAMESPACE \
+    payment $SCRIPT_DIR/../../helm/single 
+
 helm upgrade --install --namespace $NAMESPACE \
     --set serviceMonitor.enabled=true \
     --set config.datasource.host=postgres-postgresql \
@@ -31,8 +39,14 @@ helm upgrade --install --namespace $NAMESPACE \
     --set config.datasource.user=postgres \
     postgres-exporter prometheus-community/prometheus-postgres-exporter
 
-# Deploy client
-upgrade_client $NAMESPACE $SCENARIO $SCRIPT_DIR "client" "frontend" "/scenarios/$SCENARIO-plan.yaml"
+echo "Deploying client"
+helm delete --namespace $NAMESPACE client
+helm upgrade --install --namespace $NAMESPACE \
+    --set image.tag=$IMAGE_TAG \
+    --set chaos.host=frontend \
+    --set chaos.plan=/scenarios/$NAMESPACE-plan.yaml \
+    --set business_application=$NAMESPACE \
+    client $SCRIPT_DIR/../../helm/client
 
 echo "Configure scraper"
 kubectl create secret generic \
